63980ea0f5010ce050e0b04384d095863df98b90
arsham-khoee <arsham.gh97@gmail.com>
2022-12-24 16:25:34 +0330

    add main layout and routes

---
+++ src/layouts/MainLayout.jsx
@@ -0,0 +1,97 @@
+import Box from "@mui/material/Box"
+import Drawer from "@mui/material/Drawer"
+import AppBar from "@mui/material/AppBar"
+import CssBaseline from "@mui/material/CssBaseline"
+import Toolbar from "@mui/material/Toolbar"
+import List from "@mui/material/List"
+import Typography from "@mui/material/Typography"
+import ListItem from "@mui/material/ListItem"
+import ListItemButton from "@mui/material/ListItemButton"
+import ListItemIcon from "@mui/material/ListItemIcon"
+import ListItemText from "@mui/material/ListItemText"
+import PersonIcon from "@mui/icons-material/Person"
+import ApartmentIcon from "@mui/icons-material/Apartment"
+import LogoutIcon from "@mui/icons-material/Logout"
+import { Link, Outlet } from "react-router-dom"
+
+const drawerWidth = 240
+
+const navMenu = [
+  {
+    label: "Employees",
+    icon: <PersonIcon />,
+  },
+  {
+    label: "Departments",
+    icon: <ApartmentIcon />,
+  },
+  {
+    label: "Logout",
+    icon: <LogoutIcon />,
+  },
+]
+
+export default function ClippedDrawer() {
+  return (
+    <Box sx={{ display: "flex" }}>
+      <CssBaseline />
+      <AppBar
+        position="fixed"
+        sx={{ zIndex: (theme) => theme.zIndex.drawer + 1 }}
+      >
+        <Toolbar>
+          <Typography variant="h6" noWrap component="div">
+            Employee Management
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <Drawer
+        variant="permanent"
+        sx={{
+          width: drawerWidth,
+          flexShrink: 0,
+          [`& .MuiDrawer-paper`]: {
+            width: drawerWidth,
+            boxSizing: "border-box",
+          },
+        }}
+      >
+        <Toolbar />
+        <Box style={{ height: "100%" }} sx={{ overflow: "auto" }}>
+          <List
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              height: "100%",
+            }}
+          >
+            {navMenu.map((item, index) => (
+              <Link
+                style={{
+                  textDecoration: "none",
+                  color: "inherit",
+                  marginTop: index === 2 ? "auto" : "",
+                }}
+                to={item.label}
+              >
+                <ListItem
+                  key={item.label}
+                  disablePadding
+                >
+                  <ListItemButton>
+                    <ListItemIcon>{item.icon}</ListItemIcon>
+                    <ListItemText primary={item.label} />
+                  </ListItemButton>
+                </ListItem>
+              </Link>
+            ))}
+          </List>
+        </Box>
+      </Drawer>
+      <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
+        <Toolbar />
+        <Outlet />
+      </Box>
+    </Box>
+  )
+}
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,71 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from "react"
+import ReactDOM from "react-dom/client"
+import { createBrowserRouter, RouterProvider } from "react-router-dom"
+import "./index.css"
+import MainLayout from "./layouts/MainLayout"
+import DepartmentsPage from "./pages/DepartmentsPage"
+import EmployeesPage from "./pages/EmployeesPage"
+import ErrorPage from "./pages/ErrorPage"
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <MainLayout />,
+    errorElement: <ErrorPage />, // 404 page
+    children: [
+      // {
+      //   index: true, // Loads at '/'
+      //   element: <HomePage />,
+      // },
+      {
+        path: "employees",
+        element: <EmployeesPage />,
+      },
+      {
+        path: "departments",
+        element: <DepartmentsPage />,
+      },
+      // {
+      //   path: "startups",
+      //   element: <StartupsPage />,
+      // },
+      // {
+      //   path: "apply-process",
+      //   element: <ApplyPage />,
+      // },
+      // {
+      //   path: "services",
+      //   element: <ServicesPage />,
+      // },
+      // {
+      //   path: "faq",
+      //   element: <FaqPage />,
+      // },
+      // {
+      //   path: "contact-us",
+      //   element: <ContactPage />,
+      // },
+    ],
+  },
+  // {
+  //   path: "/auth",
+  //   element: <AuthLayout />,
+  //   errorElement: <ErrorPage />,
+  //   children: [
+  //     {
+  //       path: "login",
+  //       element: <LoginPage />,
+  //     },
+  //     {
+  //       path: "signup",
+  //       element: <SignupPage />,
+  //     },
+  //   ],
+  // },
+])
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <RouterProvider router={router} />
+  </React.StrictMode>
 )
