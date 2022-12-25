98dd8d0f5cccc2164cff30e199a116f10fc02943
arsham-khoee <arsham.gh97@gmail.com>
2022-12-25 12:12:20 +0330

    create employee page

--- src/pages/EmployeesPage.jsx
+++ src/pages/EmployeesPage.jsx
@@ -1,8 +1,19 @@
-import React from 'react'
-
+import { Box } from "@mui/system"
+import Button from "@mui/material/Button"
 function EmployeesPage() {
   return (
-    <div>EmployeesPage</div>
+    <>
+      <Box
+        sx={{
+          display: "flex",
+          gap: "10",
+          justifyContent: "flex-end"
+        }}
+      >
+        <Button>Import CSV</Button>
+        <Button>Add Manually</Button>
+      </Box>
+    </>
   )
 }
 
